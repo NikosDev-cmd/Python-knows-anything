547d1681cd816e6a74249fad9b0bf0c8239898e9
NikosDev <122281583+N1GOs@users.noreply.github.com>
2024-05-22 08:26:31 -0700

    Update PKA.py

--- PKA.py
+++ PKA.py
@@ -6,15 +6,15 @@ init(autoreset=True)
 
 set_lang("en")
 
-print(Fore.GREEN + "Welcome to Python Knows Anything. Please ask question (in one word).")
+print(Fore.GREEN + "Welcome to Python Knows Anything. Please ask your question.")
 
 while True:
     question = input("")
     if question.lower() == "exit":
+        print(Fore.YELLOW + "Exiting the program. Goodbye!")
         break
     try:
         print("\n", summary(question))
     except Exception as e:
         print(Fore.RED + "An error occurred: " + str(e))
-
 		
